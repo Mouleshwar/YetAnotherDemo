b35e43c4d444f4146828f085989ee90731e67d24
mouleshwar <mouli.4542@gmail.com>
2015-05-06 01:36:07 +0000

    add intial googleMap

--- app/src/main/java/com/yetanother/yetanotherdemo/MainActivity.java
+++ app/src/main/java/com/yetanother/yetanotherdemo/MainActivity.java
@@ -1,17 +1,28 @@
 package com.yetanother.yetanotherdemo;
 
+import android.location.Location;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MainActivity extends ActionBarActivity {
+
+public class MainActivity extends ActionBarActivity implements OnMapReadyCallback {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        MapFragment mapFragmet = (MapFragment) getFragmentManager()
+                .findFragmentById(R.id.google_map);
+        mapFragmet.getMapAsync(this);
     }
 
     @Override
@@ -34,5 +45,17 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        LatLng iitBombay = new LatLng(19.131612, 72.913573);
+
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(iitBombay, 13));
+
+        googleMap.addMarker(new MarkerOptions()
+                .title("IIT Bombay")
+                .snippet("Dystopia")
+                .position(iitBombay));
     }
 }
