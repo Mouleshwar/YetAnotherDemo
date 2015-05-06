213fa6c7eb28731e2d793cfb440fb141fa6d612a
mouleshwar <mouli.4542@gmail.com>
2015-05-06 03:48:25 +0000

    get frequent updates of tumtums

--- app/src/main/java/com/yetanother/yetanotherdemo/MainActivity.java
+++ app/src/main/java/com/yetanother/yetanotherdemo/MainActivity.java
@@ -1,10 +1,9 @@
 package com.yetanother.yetanotherdemo;
 
-import android.app.DownloadManager;
-import android.location.Location;
 import android.os.AsyncTask;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -12,6 +11,8 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.gson.Gson;
@@ -20,7 +21,6 @@ import com.squareup.okhttp.Request;
 import com.squareup.okhttp.Response;
 
 import java.io.IOException;
-import java.nio.channels.AsynchronousCloseException;
 
 
 public class MainActivity extends ActionBarActivity implements OnMapReadyCallback {
@@ -28,6 +28,9 @@ public class MainActivity extends ActionBarActivity implements OnMapReadyCallbac
     public String TUM_TUM_DATA_URL = "http://tumtum-iitb.org/jsonweb";
     private OkHttpClient mOkHttpClient;
     private GoogleMap mGoogleMap;
+    private GetTumTumsLocation mGetTumTumsLocation;
+    private BitmapDescriptor mGreenMarker;
+    private BitmapDescriptor mYellowMarker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,7 +40,10 @@ public class MainActivity extends ActionBarActivity implements OnMapReadyCallbac
                 .findFragmentById(R.id.google_map);
         mapFragment.getMapAsync(this);
         mOkHttpClient = new OkHttpClient();
-        new GetTumTumsLocation().execute();
+        mGetTumTumsLocation = new GetTumTumsLocation();
+        mGetTumTumsLocation.execute();
+        mGreenMarker = BitmapDescriptorFactory.fromResource(R.drawable.green);
+        mYellowMarker = BitmapDescriptorFactory.fromResource(R.drawable.yellow);
     }
 
     @Override
@@ -70,14 +76,9 @@ public class MainActivity extends ActionBarActivity implements OnMapReadyCallbac
         LatLng iitBombay = new LatLng(19.131612, 72.913573);
 
         googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(iitBombay, 15));
-
-        googleMap.addMarker(new MarkerOptions()
-                .title("IIT Bombay")
-                .snippet("Dystopia")
-                .position(iitBombay));
     }
 
-    public class GetTumTumsLocation extends AsyncTask<Void,MarkerModel,MarkerModel[]> {
+    public class GetTumTumsLocation extends AsyncTask<Void,MarkerModel,Void> {
 
         @Override
         protected void onPreExecute() {
@@ -85,51 +86,69 @@ public class MainActivity extends ActionBarActivity implements OnMapReadyCallbac
         }
 
         @Override
-        protected MarkerModel[] doInBackground(Void... params) {
+        protected Void doInBackground(Void... params) {
             Request request = new Request.Builder()
                     .url(TUM_TUM_DATA_URL)
                     .build();
-            Response response = null;
-            try {
-                response = mOkHttpClient.newCall(request).execute();
-                if(response!=null) {
-                    Gson gson = new Gson();
-                    ResponseModel responseModel = gson.fromJson(response.body().string(),
-                            ResponseModel.class);
-                    // publishProgress(responseModel.markers);
-                    return responseModel.getMarkers();
+            while(true) {
+                Log.d("MainActivity.class", "in while loop");
+                Response response = null;
+                try {
+                    response = mOkHttpClient.newCall(request).execute();
+                    if(response!=null) {
+                        Gson gson = new Gson();
+                        ResponseModel responseModel = gson.fromJson(response.body().string(),
+                                ResponseModel.class);
+                        publishProgress(responseModel.getMarkers());
+                        Thread.currentThread().sleep(5000);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
                 }
-            } catch (IOException e) {
-                e.printStackTrace();
             }
-
-            return null;
         }
 
         @Override
         protected void onProgressUpdate(MarkerModel... markers) {
-
+            updateMarkers(markers);
         }
 
         @Override
-        protected void onPostExecute(MarkerModel... markers) {
-            updateMarkers(markers);
+        protected void onPostExecute(Void params) {
+
         }
     }
 
     private void updateMarkers(MarkerModel[] markers) {
+        Log.d("MianActivity", "update markers called");
         if(mGoogleMap!=null) {
+            mGoogleMap.clear();
             for(int i=0; i<markers.length; i++){
                 MarkerModel marker = markers[i];
-                if(marker.getIdle()==1) {
-                    continue;
-                }
                 LatLng position = new LatLng(Double.parseDouble(marker.getLat()),
                         Double.parseDouble(marker.getLng()));
-                mGoogleMap.addMarker(new MarkerOptions()
+                MarkerOptions markerObject = new MarkerOptions()
                         .title(marker.getRoute())
                         .snippet(marker.getDescription())
-                        .position(position));
+                        .position(position);
+                if(marker.getIdle()==1) {
+//                    markerObject.icon(BitmapDescriptorFactory.fromResource(R.drawable.yellow));
+                    mGoogleMap.addMarker(new MarkerOptions()
+                            .title(marker.getRoute())
+                            .snippet(marker.getDescription())
+                            .position(position)
+                            .icon(mYellowMarker));
+                } else {
+//                    markerObject.icon(BitmapDescriptorFactory.fromResource(R.drawable.green));
+                    mGoogleMap.addMarker(new MarkerOptions()
+                            .title(marker.getRoute())
+                            .snippet(marker.getDescription())
+                            .position(position)
+                            .icon(mGreenMarker));
+                }
+//                mGoogleMap.addMarker(markerObject);
             }
         }
     }
